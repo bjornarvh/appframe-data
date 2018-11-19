cbf18f11e99102cef93df0960c335a95371281cf
Bjørnar Vister Hansen <bvh@olenbetong.no>
2018-11-19 19:21:40 +0100

    Set up project dependencies

---
+++ appframe-data-object.js
@@ -0,0 +1,7 @@
+import EventEmitter from 'eventemitter3';
+
+class AppframeDataObject extends EventEmitter {
+
+}
+
+export default AppframeDataObject;
---
+++ rollup.mjs
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+/* eslint-env node */
+import rollup from 'rollup';
+import minimist from 'minimist';
+import babel from 'rollup-plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
+
+const args = minimist(process.argv.slice(2));
+const baseName = 'appframe-data-object';
+
+function babelPlugin(format) {
+	if (format === 'esm') {
+		return babel({
+			presets: [
+				['@babel/preset-env', {
+					'targets': {
+						'browsers': [
+							'last 5 Chrome versions',
+							'last 5 Firefox versions',
+							'last 3 Safari versions',
+							'last 3 Edge versions'
+						]
+					},
+					'useBuiltIns': false
+				}]
+			]
+		});
+	}
+
+	return babel();
+}
+
+async function build(format) {
+	const fileExt = args.prod ? `${format}.min.js` : `${format}.js`;
+	const plugins = [
+		babelPlugin(format)
+	];
+
+	if (args.prod) {
+		plugins.push(minify());
+	}
+
+	const inputOptions = {
+		input: 'appframe-data-object.js',
+		plugins
+	};
+
+	const outputOptions = {
+		file: `dist/${baseName}.${fileExt}`,
+		format,
+		name: 'getLocalizedString'
+	};
+
+	const bundle = await rollup.rollup(inputOptions);
+
+	await bundle.write(outputOptions);
+}
+
+async function buildAll() {
+	console.log('Building ESM build...');
+	await build('esm');
+	console.log('Building UMD build...');
+	await build('umd');
+}
+
+buildAll();
