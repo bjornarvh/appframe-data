adc20bf88fe1fbdc9a1bce82829646abd7e88a8c
Bjørnar Vister Hansen <bvh@olenbetong.no>
2018-11-21 10:15:12 +0100

    Add babel plugin for class properties, and use babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
         "es6": true
     },
     "extends": "eslint:recommended",
+    "parser": "babel-eslint",
     "parserOptions": {
-        "ecmaVersion": 2017,
         "sourceType": "module"
     },
     "rules": {
