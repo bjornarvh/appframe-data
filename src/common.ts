982d3a5b473335b5b32c666d0dedd97886b22f66
Bjørnar Vister Hansen <bvh@olenbetong.no>
2018-11-23 11:06:11 +0100

    Make fireCallback return whether the callback was invoked

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,8 @@
-export function fireCallback(callback : Function | undefined, ...args : any[]) {
+export function fireCallback(callback : Function | undefined, ...args : any[]) : boolean {
 	if (typeof callback === 'function') {
 		callback(...args);
+		return true;
 	}
+
+	return false;
 }
--- test/common.test.js
+++ test/common.test.js
@@ -1,16 +1,16 @@
 const { fireCallback } = require('../src/common');
 
-test('Callback is called with correct arguments in fireCallback', () => {
-	let firstArg = '';
-	let secondArg = '';
+describe('fireCallback', () => {
+	it('calls the callback with all the arguments', () => {
+		const testCallback = jest.fn(() => null);
+		const result = fireCallback(testCallback, 'test1', 2);
 
-	function testCallback(arg1, arg2) {
-		firstArg = arg1;
-		secondArg = arg2;
-	}
-
-	fireCallback(testCallback, 'test1', 2);
-
-	expect(firstArg).toBe('test1');
-	expect(secondArg).toBe(2);
+		expect(result).toBe(true);
+		expect(testCallback).toBeCalledWith('test1', 2);
+	});
+	
+	it('returns if callback is not a function', () => {
+		const result = fireCallback(undefined, 'test2', 2);
+		expect(result).toBe(false);
+	});
 });
